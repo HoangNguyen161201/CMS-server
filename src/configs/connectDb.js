0a845b8d7acd83c35c28cbade0f6220dc6feffe7
nguyen quang hoang <hoanghpang@gmail.com>
2022-03-09 07:33:04 +0700

    update database

--- src/configs/connectDb.js
+++ src/configs/connectDb.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 
 //Function connection database
 const db = () => {
-  mongoose
-    .connect('mongodb://localhost:27017/CMS')
-    .then(() => {
-      console.log('Connect Success DB');
-    })
-    .catch((err) => {
-      console.log('Connect False DB', err);
-    });
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://admin4:kingspear1999@cluster0.trnqb.mongodb.net/CMS?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  client.connect(err => {
+    if(!err){
+      console.log('connect success')
+    }
+    const collection = client.db("test").collection("devices");
+    client.close();
+  });
 };
 
 module.exports = db
